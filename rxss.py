793be923b28c5bfbe957133685909952d0e1216b
basedygt <167074291+basedygt@users.noreply.github.com>
2024-06-21 21:01:42 +0000

    Update rxss.py

--- rxss.py
+++ rxss.py
@@ -47,7 +47,7 @@ class Rxss:
             print(error)
             return
 
-        if response and self.payload in response.text:
+        if response.text and self.payload in response.text:
             print(url)
             if self.output:
                 with open(self.output, "a") as f:
