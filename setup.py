224f4c88ee50f2d90f0579823aa8b58f92bb9c31
basedygt <167074291+basedygt@users.noreply.github.com>
2024-06-23 12:27:31 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import io
+from setuptools import setup, find_packages
+from os import path
+
+pwd = path.abspath(path.dirname(__file__))
+with io.open(path.join(pwd, 'README.md'), encoding='utf-8') as readme:
+    desc = readme.read()
+
+setup(
+    name='Rxss',
+    version='0.0.1',
+    description='Tool to check reflecting params and paths in a bunch of URLs',
+    long_description=desc,
+    long_description_content_type='text/markdown',
+    author='basedygt',
+    license='Apache-2.0 License',
+    url='https://github.com/basedygt/rxss',
+    packages=find_packages(),
+    classifiers=[
+        'Topic :: Security',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords=['rxss', 'Reflected Cross Site Scripting', 'Pentest tools'],
+    install_requires=['qsreplace'],
+    entry_points={
+        'console_scripts': [
+            'rxss = rxss:main',
+        ],
+    },
+)
